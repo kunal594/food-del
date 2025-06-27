65cfd016067af49367d79ee47d11cd953f63f05b
kunal594 <kunal.2201243cs@iiitbh.ac.in>
2025-06-28 00:52:15 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ import 'dotenv/config';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placeing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5174";
+  const frontend_url = "https://food-del-frontend-khrv.onrender.com";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
