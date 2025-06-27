b3b7b5b3cb983441b92e14e84e7ed8c19dc8e2bf
kunal594 <kunal.2201243cs@iiitbh.ac.in>
2025-06-28 00:18:35 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext =createContext(null)
 
 const StoreContextProvider = (props) => {
     const[cartItems,setCartItems] = useState({});
-    const url = "http://localhost:3000"
+    const url = "https://food-del-backend-222s.onrender.com"
     const [token,setToken] = useState("");
     const [food_list,setFoodList]= useState([])
 
