be03ffaf95419c58c4077edcb1418a183b8587d5
sharma6-web <kunalkumarsharma202@gmail.com>
2025-07-18 12:24:46 +0530

    update

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,24 +1,38 @@
-//import React from 'react'
-import React, {useContext} from 'react'
-import './FoodDisplay.css'
-import { StoreContext} from '../../context/StoreContext'
-import FoodItem from '../FoodItem/FoodItem'
-const FoodDisplay = ({category}) => {
-    const {food_list} = useContext(StoreContext)
+import React, { useContext } from 'react';
+import './FoodDisplay.css';
+import { StoreContext } from '../../context/StoreContext';
+import FoodItem from '../FoodItem/FoodItem';
+
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
+
   return (
     <div className='food-display' id='food-display'>
-        <h2>Top dishes near you</h2>
-        <div className="food-display-list">
-            {food_list?.map((item,index)=>{
-               if(category==="All" || category===item.category) {
-
-               
-                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
-               }
-            })}
-        </div>
+      <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list
+          ?.filter(
+            (item) =>
+              item &&
+              item._id &&
+              item.name &&
+              item.price !== undefined &&
+              item.image &&
+              (category === "All" || category === item.category)
+          )
+          .map((item, index) => (
+            <FoodItem
+              key={index}
+              id={item._id}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+              image={item.image}
+            />
+          ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
