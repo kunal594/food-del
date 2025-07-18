463f70ae51d44d579cf77cc775623f48389a633a
sharma6-web <kunalkumarsharma202@gmail.com>
2025-07-18 11:10:11 +0530

    update food item

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -11,11 +11,13 @@ const FoodItem = ({id,name,price,description,image}) => {
     <div className='food-item'>
         <div className="food-item-img-container">
             <img className='food-item-image' src={url+"/images/"+image} alt=""/>
-            {!cartItems[id] 
+         {cartItems && !cartItems[id]
+
                ?<img className='add' onClick={()=>addToCart(id)}  src={assets.add_icon_white} alt=""/>
                :<div className='food-item-counter'>
                <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt=""/>
-               <p>{cartItems[id]}</p>
+               <p>{cartItems?.[id]}</p>
+
                <img onClick={()=>addToCart(id)}  src={assets.add_icon_green} alt=""/>
                </div>
 
