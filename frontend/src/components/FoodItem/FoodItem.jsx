234791dcfb8d7d18fbcdbdd4bb40e72cd60c9277
sharma6-web <kunalkumarsharma202@gmail.com>
2025-07-18 12:16:16 +0530

    update

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -11,17 +11,15 @@ const FoodItem = ({id,name,price,description,image}) => {
     <div className='food-item'>
         <div className="food-item-img-container">
             <img className='food-item-image' src={url+"/images/"+image} alt=""/>
-         {cartItems && !cartItems[id]
+        {!cartItems?.[id]
+  ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
+  : <div className='food-item-counter'>
+      <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+      <p>{cartItems[id]}</p>
+      <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
+    </div>
+}
 
-               ?<img className='add' onClick={()=>addToCart(id)}  src={assets.add_icon_white} alt=""/>
-               :<div className='food-item-counter'>
-               <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt=""/>
-               <p>{cartItems?.[id]}</p>
-
-               <img onClick={()=>addToCart(id)}  src={assets.add_icon_green} alt=""/>
-               </div>
-
-            }
         </div>
         <div className="food-item-info">
             <div className="food-item-name-rating">
